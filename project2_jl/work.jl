5c83e9d743c1c1820132d3062110de56f3d93d47
Juliet Nwagwu Ume-Ezeoke <julietnwagwuume-ezeoke@DN51skp8.SUNet>
2022-04-20 22:17:03 -0700

    constraints

--- project2_jl/work.jl
+++ project2_jl/work.jl
@@ -3,6 +3,21 @@ include("helpers.jl")
 using Plots
 
 
+function plot_constraint(c, constraint_num)
+    println(constraint_num)
+    c1 = []
+    # square matrix, so s1 and s2 will be the same
+    s1 = size(c, 1)
+    for row = 1:s1
+        for col = 1:s1
+            push!(c1, c[row, col][constraint_num])
+        end
+    end
+    c1 = reshape(c1, s1, s1)';
+    c1_filt = ifelse.(c1 .< 0, c1, 0);
+    contour!(xr, yr, c1_filt, xlims=(-3,3), ylims=(-3,3),aspectratio=:equal, colorbar_entry=false, linecolor=cgrad(:greys))
+end
+
 function make_contour_plot(probname)
     println("making a contour plot")
     # visualize function problem
@@ -12,7 +27,13 @@ function make_contour_plot(probname)
     yr = Vector(-3:0.1:3)
     z = [prob.f([a,b]) for a=xr, b=yr]
     
-    c_plot = contour!(xr, yr, z, xlims=(-3,3), ylims=(-3,3),aspectratio=:equal)
+    c_plot = contour(xr, yr, z, xlims=(-3,3), ylims=(-3,3),aspectratio=:equal)
+
+    # visialize constraints 
+    c = [prob.c([a,b]) for a=xr, b=yr]
+    for i = 1:length(c[1,1])
+        plot_constraint(c, i)
+    end
 
     return c_plot
 
